93825a043659bb98ecb9805e56a5f88e2e801098
Crazy-C0der <sh2000.cool.j@gmail.com>
2020-05-26 18:38:15 +0530

    commit m

---
+++ login.js
@@ -0,0 +1,27 @@
+function login()
+{
+var email=document.getElementById("uid");
+var pswd=document.getElementById("pwd");
+location.href = "C:\Users\SHRUTI\Documents\ISOC\kome.html"
+};
+
+function new_user()
+{
+    var email=document.getElementById("");
+    var name=document.getElementById("");
+    var id=document.getElementById("");
+    var pswd=document.getElementById("");
+    location.href = "C:\Users\SHRUTI\Documents\ISOC\kome.html";
+};
+
+function forgot_password()
+{
+console.log("Forgot Password")
+};
+
+function select(string)
+{
+   var ele=getElementById(string);
+   ele
+}
+
